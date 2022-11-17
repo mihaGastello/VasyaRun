e3f4f6ecd89398fd46ece1c3931b671925aa080e
Kisluhin Mihail <89089003390@mail.ru>
2022-11-17 13:41:34 +0400

    background

--- VasyaRun/GameScene.swift
+++ VasyaRun/GameScene.swift
@@ -10,79 +10,44 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // Texture
+    var bgTexture: SKTexture!
+    
+    // SpriteNodes
+    var bg = SKSpriteNode()
+    
+    // Sprite Objects
+    var bgObject = SKNode()
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        bgTexture = SKTexture(imageNamed: "bg01.jpg")
+        createObjects()
+        createGame()
         }
+    
+    func createObjects() {
+        self.addChild(bgObject)
+    }
+    
+    func createGame() {
+        createBg()
+    }
+    
+    func createBg() {
+        bgTexture = SKTexture(imageNamed: "bg01.jpg")
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let moveBg = SKAction.moveBy(x: -bgTexture.size().width, y: 0, duration: 3)
+        let replaceBg = SKAction.moveBy(x: bgTexture.size().width, y: 0, duration: 0)
+        let moveBgForever = SKAction.repeatForever(SKAction.sequence([moveBg, replaceBg]))
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        for i in 0..<3 {
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: size.width/4 + bgTexture.size().width * CGFloat(i), y: size.height/2.0)
+            bg.size.height = self.frame.height
+            bg.run(moveBgForever)
+            bg.zPosition = -1
+            bgObject.addChild(bg)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
