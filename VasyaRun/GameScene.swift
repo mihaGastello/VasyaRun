2f2c4a6c02ab9ce19b6a2edb76a383856f489a85
Kisluhin Mihail <89089003390@mail.ru>
2022-11-17 16:48:59 +0400

    hero

--- VasyaRun/GameScene.swift
+++ VasyaRun/GameScene.swift
@@ -12,25 +12,35 @@ class GameScene: SKScene {
     
     // Texture
     var bgTexture: SKTexture!
+    var flyHeroTexture: SKTexture!
     
     // SpriteNodes
     var bg = SKSpriteNode()
+    var hero = SKSpriteNode()
     
     // Sprite Objects
     var bgObject = SKNode()
+    var heroObject = SKNode()
+    
+    // Array textures for animate
+    var heroFlyTexturesArray = [SKTexture]()
     
     override func didMove(to view: SKView) {
         bgTexture = SKTexture(imageNamed: "bg01.jpg")
+        flyHeroTexture = SKTexture(imageNamed: "run_000.png")
+        
         createObjects()
         createGame()
         }
     
     func createObjects() {
         self.addChild(bgObject)
+        self.addChild(heroObject)
     }
     
     func createGame() {
         createBg()
+        createHero()
     }
     
     func createBg() {
@@ -47,7 +57,36 @@ class GameScene: SKScene {
             bg.run(moveBgForever)
             bg.zPosition = -1
             bgObject.addChild(bg)
-            
         }
+    }
+    
+    func addHero(heroNode: SKSpriteNode, atPosition position: CGPoint) {
+        hero = SKSpriteNode(texture: flyHeroTexture)
+        
+        // Animate hero
+        heroFlyTexturesArray = [
+            SKTexture(imageNamed: "run_000.png"),
+            SKTexture(imageNamed: "run_004.png"),
+            SKTexture(imageNamed: "run_008.png"),
+            SKTexture(imageNamed: "run_012.png"),
+            SKTexture(imageNamed: "run_016.png"),
+            SKTexture(imageNamed: "run_020.png"),
+            SKTexture(imageNamed: "run_024.png"),
+            SKTexture(imageNamed: "run_028.png"),
+            SKTexture(imageNamed: "run_032.png"),
+            SKTexture(imageNamed: "run_036.png"),
+            SKTexture(imageNamed: "run_040.png")]
+        
+        let heroFlyAnimation = SKAction.animate(with: heroFlyTexturesArray, timePerFrame: 0.1)
+        let flyHero = SKAction.repeatForever(heroFlyAnimation)
+        hero.run(flyHero)
+        
+        hero.position = position
+        heroObject.addChild(hero)
+    }
+    
+    func createHero() {
+        addHero(heroNode: hero, atPosition: CGPoint(x: self.size.width/4, y: 0 + flyHeroTexture.size().height + 400))
+       // hero.zPosition = -2 // kisluhin
     }
 }
--- VasyaRun/GameViewController.swift
+++ VasyaRun/GameViewController.swift
@@ -18,7 +18,7 @@ class GameViewController: UIViewController {
             // Load the SKScene from 'GameScene.sks'
             let scene = GameScene(size: CGSize(width: 1024, height: 768))
                 // Set the scale mode to scale to fit the window
-                //scene.scaleMode = .aspectFill
+                //scene.scaleMode = .aspectFill // kisluhin
                   scene.scaleMode = .resizeFill
                 
                 // Present the scene
